a6282d5322fd2357a8a18f40bd222b75fe431da4
Duy <dennisdnguyen19@gmail.com>
2017-04-01 02:02:41 -0400

    Added Main java files

---
+++ app/src/main/java/com/dragonfury/duy/p4pt03nguyendenniscustomapp3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dragonfury.duy.p4pt03nguyendenniscustomapp3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new DrawView(this));
+    }
+}
+
